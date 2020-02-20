4a6ce0a2a1f94947c942655ed8b916745286245c
abhishekworkspace <36187249+abhishekworkspace@users.noreply.github.com>
2020-02-20 15:54:59 +0530

    Update 1.1
    FFT changes to implementation for smart energy project

--- FFT.py
+++ FFT.py
@@ -1,1 +1,15 @@
-import 
+import numpy as np
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+x = np.random.random(1024)
+np.allclose(DFT_slow(x), np.fft.fft(x))
+
+%timeit DFT_slow(x)
+%timeit np.fft.fft(x) 
